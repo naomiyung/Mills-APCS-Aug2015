5b26605c7db3b6d9c63a96f8b25a44862169aba3
naomiyung <16yungn55@millshighsch.org>
2015-08-22 11:21:34 -0700

    8/22/15 Hello World Demonstration
    This program was basic but made me excited to learn more computer
    science.

---
+++ Naomi Projects/Hello_World.java
@@ -0,0 +1,14 @@
+
+/**
+ * Hello_World program.
+ * 
+ * Naomi Yung 
+ * 8/22/15 V1
+ */
+public class Hello_World
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World! From Naomi"); //Display the string
+    }
+}
